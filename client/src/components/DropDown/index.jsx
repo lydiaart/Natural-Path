f03f40b51fe8bafc594429a94851fd7a726ac07a
Lydia Loy <lydia_art@yahoo.com>
2022-02-28 11:56:51 -0600

    changed dropdown to select a category

--- client/src/components/DropDown/index.jsx
+++ client/src/components/DropDown/index.jsx
@@ -3,7 +3,8 @@ function DropDown() {
         <>
             <div class="dropdown">
                 <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    Dropdown
+                    Select a category
+                    
                     <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
