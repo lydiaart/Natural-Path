d29bb41e4d72eeb8b5c261d48f09cc1c587c6a72
Nathan Batchelder <natebatchelder@gmail.com>
2022-02-27 16:39:32 -0800

    initial commit

--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -1,51 +1,47 @@
-import Work from '../../components/Work';
-import serviceData from '../../utils/service.json';
+import Work from "../../components/Work";
+import serviceData from "../../utils/service.json";
 
 function Service() {
-    return (
-        <>
-            {/* <!--project start--> */}
-            <section id="project" className="project">
-                <div className="container">
-                    <div className="project-details">
-                        <div className="project-header text-left">
-                            <h2>Service</h2>
-                            <p>
-                                Please choose the service you wish.
-                            </p>
-                        </div>
-                        {/* <!--/.project-header--> */}
-                        <div className="project-content">
-                            <div className="gallery-content">
-                                <div className="isotope">
-                                <div className="row">
-												<div className="col-sm-6 col-xs-12">
-													<div className="item">
-														<Work serviceData={serviceData[4]} />
-														{/* <!-- /.isotope-overlay --> */}
-													</div>
-													{/* <!-- /.item --> */}
-												</div>
-												{/* <!-- /.col --> */}
-												<div className="col-sm-6 col-xs-12">
-													<div className="item">
-														<Work serviceData={serviceData[5]} />
-														{/* <!-- /.isotope-overlay --> */}
-													</div>
-													{/* <!--/.item --> */}
-												</div>
-												{/* <!-- /.col --> */}
-											</div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <>
+      {/* <!--project start--> */}
+      <section id="project" className="project">
+        <div className="container">
+          <div className="project-details">
+            <div className="project-header text-left">
+              <h2>Service</h2>
+              <p>Please choose the service you wish.</p>
+            </div>
+            {/* <!--/.project-header--> */}
+            <div className="project-content">
+              <div className="gallery-content">
+                <div className="isotope">
+                  <div className="row">
+                    <div className="col-sm-6 col-xs-12">
+                      <div className="item">
+                        <Work serviceData={serviceData[4]} />
+                        {/* <!-- /.isotope-overlay --> */}
+                      </div>
+                      {/* <!-- /.item --> */}
                     </div>
+                    {/* <!-- /.col --> */}
+                    <div className="col-sm-6 col-xs-12">
+                      <div className="item">
+                        <Work serviceData={serviceData[5]} />
+                        {/* <!-- /.isotope-overlay --> */}
+                      </div>
+                      {/* <!--/.item --> */}
+                    </div>
+                    {/* <!-- /.col --> */}
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-
-
 
 export default Service;
