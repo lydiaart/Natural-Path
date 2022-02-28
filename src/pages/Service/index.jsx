9a900118002a65879c5a24cc398ce3a49b11b066
Nathan Batchelder <natebatchelder@gmail.com>
2022-02-27 19:40:33 -0800

    edits to service index.jsx for formatting

--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -10,10 +10,20 @@ function Service() {
           <div className="project-details">
             <div className="project-header text-left">
               <h2>Our Offerings</h2>
-              <p>Let's begin your <i>Natural Path</i> journey</p>
+              <p>
+                Let's begin your <i>Natural Path</i> journey
+              </p>
             </div>
-            
           </div>
+          {/* one row with three boxes */}
+          {/* <!--/.row--> */}
+          <div class="container"></div>
+          <div class="project-details">
+            <p>Provider 1</p>
+          </div>
+            <div class="column"></div>
+            <div class="column"></div>
+          {/* one row with a select feature */}
         </div>
       </section>
     </>
