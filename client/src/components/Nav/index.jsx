44a8db7adfdb34768c8b401cb9bf64e301f1792e
Carter Supple <Cartersupple123@gmail.com>
2022-03-02 18:10:49 -0800

    fixed nav and updated when logged in

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -2,91 +2,154 @@ import "./index.css";
 import Auth from "../../utils/auth";
 import React from "react";
 function Nav() {
-  return (
-    <>
-      <section id="menu">
-        <div className="container">
-          <a className="navbar-brand" id="name">
-            {" "}
-            <img src="assets/images/logo/logo.png" alt="logo" />{" "}
-          </a>
-          <div className="menubar">
-            <nav className="navbar navbar-default">
-              {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-              <div className="navbar-header">
-                <button
-                  type="button"
-                  className="navbar-toggle collapsed"
-                  data-toggle="collapse"
-                  data-target="#bs-example-navbar-collapse-1"
-                  aria-expanded="false"
+  if (Auth.loggedIn()) {
+    return (
+      <>
+        <section id="menu">
+          <div className="container">
+            <a className="navbar-brand" id="name">
+              {" "}
+              <img src="assets/images/logo/logo.png" alt="logo" />{" "}
+            </a>
+            <div className="menubar">
+              <nav className="navbar navbar-default">
+                {/* <!-- Brand and toggle get grouped for better mobile display --> */}
+                <div className="navbar-header">
+                  <button
+                    type="button"
+                    className="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false"
+                  >
+                    <span className="sr-only">Toggle navigation.</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                </div>
+                {/* <!--/.navbar-header --> */}
+
+                {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+                <div
+                  className="collapse navbar-collapse"
+                  id="bs-example-navbar-collapse-1"
                 >
-                  <span className="sr-only">Toggle navigation.</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-              </div>
-              {/* <!--/.navbar-header --> */}
+                  <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+                    <li>
+                      <a href="/about">About</a>
+                    </li>
+                    <li>
+                      <a href="/services">Service</a>
+                    </li>
+                    <li>
+                      <a href="/product">Products</a>
+                    </li>
+                    <li>
+                      <a href="/contact">Contact</a>
+                    </li>
+                    <li>
+                      <a href="/profile">Profile</a>
+                    </li>
+                    <li>
+                      <a href="#" onClick={()=>Auth.logout()}>Sign Out</a>
+                    </li>
 
-              {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
-              <div
-                className="collapse navbar-collapse"
-                id="bs-example-navbar-collapse-1"
-              >
-                <ul className="nav navbar-nav navbar-right">
-                  <li>
-                    <a href="/">Home</a>
-                  </li>
-                  <li>
-                    <a href="/about">About</a>
-                  </li>
-                  <li>
-                    <a href="/services">Service</a>
-                  </li>
-                  <li>
-                    <a href="/product">Products</a>
-                  </li>
-                 
-                  <li>
-                    <a href="/contact">Contact</a>
-                  </li>
-                  <li>
-                    <a href="/SignUp">Sign Up</a>
-                  </li>
-                  <li>
-                    <a href="/Login">Login</a>
-                  </li>
-                  <li>
+                    <li>
+                      <a href="/cart">
+                        <i class="fa-solid fa-cart-plus"></i> (1) Cart
+                      </a>
                     </li>
-                    <li> {function requireAuth(nextState, replace, next) {
-                      if (!Auth.loggedIn) {
-                        replace({
-                          pathname: "/Profile",
-                          state: { nextPathname: nextState.location.pathname },
-                        });
-                      }
-                      next();
-                    }}
-                    </li>
-                  <li>
-                    <a href="/cart">
-                      <i class="fa-solid fa-cart-plus"></i> (1) Cart
-                    </a>
-                  </li>
-                </ul>
-                {/* <!-- / ul --> */}
-              </div>
-              {/* ><!-- /.navbar-collapse --> */}
-            </nav>
-            {/* <!--/nav --> */}
+                  </ul>
+                  {/* <!-- / ul --> */}
+                </div>
+                {/* ><!-- /.navbar-collapse --> */}
+              </nav>
+              {/* <!--/nav --> */}
+            </div>
+            {/* <!--/.menubar --> */}
           </div>
-          {/* <!--/.menubar --> */}
-        </div>
-        {/* <!-- /.container --> */}
-      </section>
-    </>
-  );
-}
+          {/* <!-- /.container --> */}
+        </section>
+      </>
+    );
+  } else if (!Auth.loggedIn()) {
+    return (
+      <>
+        <section id="menu">
+          <div className="container">
+            <a className="navbar-brand" id="name">
+              {" "}
+              <img src="assets/images/logo/logo.png" alt="logo" />{" "}
+            </a>
+            <div className="menubar">
+              <nav className="navbar navbar-default">
+                {/* <!-- Brand and toggle get grouped for better mobile display --> */}
+                <div className="navbar-header">
+                  <button
+                    type="button"
+                    className="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false"
+                  >
+                    <span className="sr-only">Toggle navigation.</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                </div>
+                {/* <!--/.navbar-header --> */}
 
+                {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+                <div
+                  className="collapse navbar-collapse"
+                  id="bs-example-navbar-collapse-1"
+                >
+                  <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+                    <li>
+                      <a href="/about">About</a>
+                    </li>
+                    <li>
+                      <a href="/services">Service</a>
+                    </li>
+                    <li>
+                      <a href="/product">Products</a>
+                    </li>
+
+                    <li>
+                      <a href="/contact">Contact</a>
+                    </li>
+                    <li>
+                      <a href="/SignUp">SignUp</a>
+                    </li>
+                    <li>
+                      <a href="/Login">Login</a>
+                    </li>
+                    <li>
+                      <a href="/cart">
+                        <i class="fa-solid fa-cart-plus"></i> (1) Cart
+                      </a>
+                    </li>
+                  </ul>
+                  {/* <!-- / ul --> */}
+                </div>
+                {/* ><!-- /.navbar-collapse --> */}
+              </nav>
+              {/* <!--/nav --> */}
+            </div>
+            {/* <!--/.menubar --> */}
+          </div>
+          {/* <!-- /.container --> */}
+        </section>
+      </>
+    );
+  }
+}
 export default Nav;
