2ef98eb2ca3fc08675ccbb94f0e042f54f6a8588
Lydia Loy <lydia_art@yahoo.com>
2022-03-05 14:35:19 -0600

    updated cart count

--- client/src/pages/Cart/index.jsx
+++ client/src/pages/Cart/index.jsx
@@ -1,6 +1,54 @@
 import './index.css';
+import { CARTS } from "../../utils/queries";
+import { useQuery } from "@apollo/client"
 
 function Cart() {
+    const { loading, data } = useQuery(CARTS)
+    const carts = data?.carts.carts || []
+    console.log(carts)
+    const displayCart = () => {
+       let quantity = 0
+       let previousProduct = ""
+
+
+     return   carts.map(cart => {
+               
+        quantity = 0
+              carts.forEach(cartItem=>{
+                  console.log(cart.name, cartItem.name)
+                  if(cart.name.trim()===cartItem.name.trim()){
+                      quantity=quantity + 1
+                  }
+                  else {
+                      quantity=1
+                  }
+              })
+
+          console.log(quantity)
+            return (
+                <tr>
+                    <td className="col-sm-8 col-md-6">
+                        <div className="media">
+                            <a className="thumbnail pull-left" href="#"> <img className="media-object" src={"/assets/images/background/" + cart.image} style={{ width: "72px", height: "72px" }} /> </a>
+                            <div className="media-body">
+                                <h4 className="media-heading"><a href="#">{cart.name}</a></h4>
+                                {/* <h5 className="media-heading"> by <a href="#">Brand name</a></h5> */}
+                                <span>Status: </span><span className="text-success"><strong>In Stock</strong></span>
+                            </div>
+                        </div></td>
+                    <td className="col-sm-1 col-md-1" style={{ textAlign: "center" }}>
+                        <input type="text" className="form-control" id="quantity" value={quantity} />
+                    </td>
+                    <td className="col-sm-1 col-md-1 text-center"><strong>${cart.price}</strong></td>
+                    <td className="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
+                    <td className="col-sm-1 col-md-1">
+                        <button type="button" className="btn btn-danger">
+                            <span className="glyphicon glyphicon-remove"></span> Remove
+                        </button></td>
+                </tr>
+            )
+        })
+    }
     return (
         <>
             <div className="container m-3">
@@ -17,27 +65,10 @@ function Cart() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td className="col-sm-8 col-md-6">
-                                        <div className="media">
-                                            <a className="thumbnail pull-left" href="#"> <img className="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style={{width: "72px", height: "72px" }}/> </a>
-                                            <div className="media-body">
-                                                <h4 className="media-heading"><a href="#">Product name</a></h4>
-                                                <h5 className="media-heading"> by <a href="#">Brand name</a></h5>
-                                                <span>Status: </span><span className="text-success"><strong>In Stock</strong></span>
-                                            </div>
-                                        </div></td>
-                                    <td className="col-sm-1 col-md-1" style={{textAlign: "center"}}>
-                                        <input type="email" className="form-control" id="exampleInputEmail1" value="3" />
-                                    </td>
-                                    <td className="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
-                                    <td className="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
-                                    <td className="col-sm-1 col-md-1">
-                                        <button type="button" className="btn btn-danger">
-                                            <span className="glyphicon glyphicon-remove"></span> Remove
-                                        </button></td>
-                                </tr>
-                                <tr>
+                                {loading ? "loading.." : displayCart() }
+
+
+                                {/* <tr>
                                     <td className="col-md-6">
                                         <div className="media">
                                             <a className="thumbnail pull-left" href="#"> <img className="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style={{width: "72px", height: "72px"}} /> </a>
@@ -57,6 +88,7 @@ function Cart() {
                                             <span className="glyphicon glyphicon-remove"></span> Remove
                                         </button></td>
                                 </tr>
+                              */}
                                 <tr>
                                     <td>   </td>
                                     <td>   </td>
