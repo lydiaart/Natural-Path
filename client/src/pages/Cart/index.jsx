b4be7658d8ca7b420089ab73e7603bf2177f6647
Lydia Loy <lydia_art@yahoo.com>
2022-03-06 00:44:12 -0600

    fix total

--- client/src/pages/Cart/index.jsx
+++ client/src/pages/Cart/index.jsx
@@ -2,12 +2,12 @@ import './index.css';
 import { CARTS } from "../../utils/queries";
 import {REMOVE_CART} from '../../utils/mutations'
 import { useQuery,useMutation } from "@apollo/client"
-import {useState} from 'react'
+ 
 import Auth from "../../utils/auth"
 function Cart() {
     const { loading, data } = useQuery(CARTS)
     const carts = data?.carts.carts || []
-    const [total,setTotal] =useState(1)
+    
 
     const [removeCart] = useMutation(REMOVE_CART)
     const handleRemove= async(cart)=>{
@@ -16,6 +16,8 @@ function Cart() {
         }})
         window.location.reload()
     }
+    let newtotal=1
+    let sum=1
     const displayCart = () => {
 
         let uniqueCart = []
@@ -37,10 +39,10 @@ function Cart() {
 
         let newCart=[]
         let i=0
-        let newtotal=1
+   
         for ( var property in uniqueQty ) {
               if(property===uniqueCart[i].name){
-               // newtotal=newtotal+uniqueCart[i].price * uniqueQty[property]
+              newtotal=newtotal+uniqueCart[i].price * uniqueQty[property]
                  newCart.push({
                      ...uniqueCart[i],
                      quantity: uniqueQty[property]
@@ -49,8 +51,9 @@ function Cart() {
               }   
               i++  
         }
-      
-      //  setTotal(newtotal)
+      console.log(newtotal)
+      // setTotal(newtotal)
+      sum=newtotal+6.94
         
         return  newCart.map(cart => {
                  return (
@@ -100,33 +103,13 @@ function Cart() {
                                 {loading ? "loading.." : displayCart()}
 
 
-                                {/* <tr>
-                                    <td className="col-md-6">
-                                        <div className="media">
-                                            <a className="thumbnail pull-left" href="#"> <img className="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style={{width: "72px", height: "72px"}} /> </a>
-                                            <div className="media-body">
-                                                <h4 className="media-heading"><a href="#">Product name</a></h4>
-                                                <h5 className="media-heading"> by <a href="#">Brand name</a></h5>
-                                                <span>Status: </span><span className="text-warning"><strong>Leaves warehouse in 2 - 3 weeks</strong></span>
-                                            </div>
-                                        </div></td>
-                                    <td className="col-md-1" style={{textAlign: "center"}}>
-                                        <input type="email" className="form-control" id="exampleInputEmail1" value="2" />
-                                    </td>
-                                    <td className="col-md-1 text-center"><strong>$4.99</strong></td>
-                                    <td className="col-md-1 text-center"><strong>$9.98</strong></td>
-                                    <td className="col-md-1">
-                                        <button type="button" className="btn btn-danger">
-                                            <span className="glyphicon glyphicon-remove"></span> Remove
-                                        </button></td>
-                                </tr>
-                              */}
+                          
                                 <tr>
                                     <td>   </td>
                                     <td>   </td>
                                     <td>   </td>
                                     <td><h5>Subtotal</h5></td>
-                                    <td className="text-right"><h5><strong>${total}</strong></h5></td>
+                                    <td className="text-right"><h5><strong>${newtotal.toFixed(2)}</strong></h5></td>
                                 </tr>
                                 <tr>
                                     <td>   </td>
@@ -140,7 +123,7 @@ function Cart() {
                                     <td>   </td>
                                     <td>   </td>
                                     <td><h3>Total</h3></td>
-                                    <td className="text-right"><h3><strong>$31.53</strong></h3></td>
+                                    <td className="text-right"><h3><strong>${sum.toFixed(2)}</strong></h3></td>
                                 </tr>
                                 <tr>
                                     <td>   </td>
