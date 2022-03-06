1253ca157adadca472939f10e9ffbaeb2efb69b8
Lydia Loy <lydia_art@yahoo.com>
2022-03-06 00:52:35 -0600

    delete develop

--- client/src/components/Header/index.jsx
+++
@@ -1,8 +0,0 @@
-function Header(props) {
-    return <>
-{/* make the header the parent wrapper so that it wraps all children inside of the header*/}
-    {props.children}
-    </>
-}
-
-export default Header;
--- client/src/components/Main/index.jsx
+++
@@ -1,8 +0,0 @@
-function main(props) {
-    return <> 
-    {/* make the header the parent wrapper so that it wraps all children inside of the header*/}
-    {props.children}
-    </>
-}
-
-export default main;
